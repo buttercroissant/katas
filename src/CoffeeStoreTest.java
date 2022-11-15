72b9c03339bbebc5239ce56a0cd056a72ed5f4f2
Melvin <79887208+buttercroissant@users.noreply.github.com>
2022-11-15 19:17:30 +0800

    feat: CoffeeStore

---
+++ src/CoffeeStoreTest.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+class CoffeeStoreTest {
+    public static void main(String[] args) {
+        CoffeeStore storeOne = new CoffeeStore();
+
+        storeOne.setStoreName("starbucks");
+        storeOne.setCoffeeBlend("java");
+        storeOne.setCoffeeBlend("sumatra");
+        storeOne.setCoffeeBlend("darkroast");
+
+        ArrayList<String> storeOneAvailableBlends = storeOne.getCoffeeBlends();
+
+        ArrayList<String> testBlends = new ArrayList<String>();
+        testBlends.add("java");
+        testBlends.add("sumatra");
+        testBlends.add("darkroast");
+
+        if(storeOneAvailableBlends.equals(testBlends)) {
+            System.out.println("Passed (Object value check)");
+        } else {
+            System.out.println("Failed (Object value check)");
+        }
+
+        if(storeOneAvailableBlends != testBlends) {
+            System.out.println("Passed (Object reference check)");
+        } else {
+            System.out.println("Failed (Object reference check)");
+        }
+         
+    }
+}
