f2618a6f996e353598e399be810008d1da72007b
buttercroissant <79887208+buttercroissant@users.noreply.github.com>
2023-12-13 16:51:23 +0800

    Event loop

---
+++ javascript/nodejs/eventloop/index.js
@@ -0,0 +1,11 @@
+// Event loop
+// - Synchronous operations comes before Asynchronous operations
+
+Promise.resolve("").then((_) => console.log("A"));
+
+setTimeout(() => console.log("B"), 0);
+
+// Tick - every time the event loop takes a full trip
+process.nextTick(() => console.log("D"));
+
+console.log("C");
